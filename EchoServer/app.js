56745053b2b75b4dfab19e76fb2e022ffad099eb
MrLokans <trikster1911@gmail.com>
2015-11-12 16:28:39 +0300

    'once' usage commit

--- EchoServer/app.js
+++ EchoServer/app.js
@@ -1,6 +1,7 @@
 var net = require('net');
 
 var server = net.createServer(function(socket){
+    // 'once' event listener may be used to respond only one time
     socket.on('data', function(data){
         socket.write(data);
     });
